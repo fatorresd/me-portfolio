c2eab0f06e69e716f326bbb351d2ebd7dd49834d
Fatorres <ftorres2018@alu.uct.cl>
2025-02-13 12:44:02 -0300

    ultimos detalle

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,16 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "../pages/Home";
-import About from "../components/About";
-import Projects from "../pages/Projects";
+import { BrowserRouter as Router, Routes } from "react-router-dom";
+
+
 
 const AppRouter: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
+
       </Routes>
     </Router>
   );
